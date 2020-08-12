4b7d2ec0230334f869eb0c3392b5f4be740ccd4b
jibalbinot <jibalbinot@DESKTOP-3JM5CK8>
2020-08-12 09:47:58 -0300

    Exercício do capítulo 4

--- AulasPOO/src/br/com/jeysonn/git/Principal.java
+++ AulasPOO/src/br/com/jeysonn/git/Principal.java
@@ -13,9 +13,22 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Alo Brasil!");
-		System.out.println("Aqui tem educação!");
-		
+		Conta c1 = new Conta();
+
+        c1.titular = "Hugo";
+        c1.numero = 123;
+        c1.agencia = "45678-9";
+        c1.saldo = 50.0;
+        c1.dataDeAbertura = "12/08/2020";
+        
+        System.out.println(c1.recuperaDadosParaImpressao());
+
+        c1.deposita(100.0);
+        
+        System.out.println(c1.recuperaDadosParaImpressao());
+        
+        System.out.println("saldo atual:" + c1.saldo);
+        System.out.println("rendimento mensal:" + c1.calculaRendimento());	
 
 	}
 
