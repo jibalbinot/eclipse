86b184ae5c538fc32d76c4b7dcaac9d939ffd71e
jibalbinot <69584514+jibalbinot@users.noreply.github.com>
2020-08-12 09:04:48 -0300

    Arquivos iniciais adicionados via upload

---
+++ AulasPOO/src/br/com/jeysonn/git/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.jeysonn.git;
+
+/**
+ * @author jibalbinot
+ * Exemplo de integração do Eclipse como GitHub
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Alo Brasil!");
+
+	}
+
+}
